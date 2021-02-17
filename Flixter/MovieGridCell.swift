41227c478169ec904c226cf6f7e52557ee1099d4
Carmen-Wu <56771256+Carmen-Wu@users.noreply.github.com>
2021-02-17 02:48:44 -0500

    PArt 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Carmen Wu  on 2/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
